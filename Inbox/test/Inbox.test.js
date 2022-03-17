48bda0ec23ad2e2b0e35df23b7f6066bbc56aeff
luongbs01 <luongbs01@gmail.com>
2022-03-17 15:55:01 +0700

    npm run test

--- Inbox/test/Inbox.test.js
+++ Inbox/test/Inbox.test.js
@@ -3,7 +3,7 @@ const ganache = require("ganache-cli");
 const Web3 = require("web3");
 const provider = ganache.provider();
 const web3 = new Web3(provider);
-const { abi, evm} = require("../compile")
+const { abi, evm } = require("../compile");
 
 const message = "Hi there!";
 let accounts;
@@ -11,7 +11,26 @@ let inbox;
 
 beforeEach(async () => {
     accounts = await web3.eth.getAccounts();
-    inbox = await web3.eth.Contract(abi)
+    inbox = await new web3.eth.Contract(abi)
         .deploy({ data: "0x" + evm.bytecode.object, arguments: [message] })
         .send({ from: accounts[0], gas: "1000000" });
+});
+
+describe("Inbox", () => {
+    it("deploys a contract", () => {
+        assert.ok(inbox.options.address);
+    });
+
+    it("has a default message", async () => {
+        const msg = await inbox.methods.message().call();
+        assert.strictEqual(msg, message);
+    });
+
+    it("can change the message", async () => {
+        const newMsg = "bye";
+        await inbox.methods.setMessage(newMsg).send({ from: accounts[0] });
+
+        const msg = await inbox.methods.message().call();
+        assert.strictEqual(msg, newMsg);
+    });
 });
