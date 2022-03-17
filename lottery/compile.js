2d14bdb76fc078fb6a8c5c75ad0b1f9a1168d6b8
luongbs01 <luongbs01@gmail.com>
2022-03-17 18:23:34 +0700

    compile.js

---
+++ lottery/compile.js
@@ -0,0 +1,29 @@
+const path = require("path");
+const fs = require("fs");
+const solc = require("solc");
+
+const lotteryPath = path.resolve(__dirname, "contracts", "Lottery.sol");
+const source = fs.readFileSync(lotteryPath, "utf8");
+
+const input = {
+    language: "Solidity",
+    sources: {
+        "Lottery.sol": {
+            content: source
+        }
+    },
+    settings: {
+        metadata: {
+            useLiteralContent: true
+        },
+        outputSelection: {
+            "*": {
+                "*": ["*"]
+            }
+        }
+    }
+};
+
+const output = JSON.parse(solc.compile(JSON.stringify(input)));
+
+module.exports = output.contracts["Lottery.sol"].Lottery;
